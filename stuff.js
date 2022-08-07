9e6b301cb319fa6beac8a6f1f455d287ad7d4b96
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2022-08-07 16:58:11 +0200

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,4 +1,9 @@
 $(window).on("load",function(){
     $(".loader").fadeOut(1000);
-    $(".content").fadeIn(1000);
+    delay(1000).then(() => $(".content").fadeIn(1000));
 })
+
+// Adds delay
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
